b6056919f0f37f7bf2777630f6d258bc10fae1c9
AcceleratorMX <redfield242@gmail.com>
2025-03-06 17:35:40 +0200

    Added unit tests from Factory Fethod pattern

---
+++ lab-2/Patterns.Tests/FactoryMethodTests.cs
@@ -0,0 +1,97 @@
+﻿using FactoryMethod.Enums;
+using FactoryMethod.Factories;
+using FactoryMethod.Subscriptions;
+using static FactoryMethod.Enums.SubscriptionType;
+
+namespace FactoryMethod.Test;
+
+public class FactoryMethodTests
+{
+    [Fact]
+    public void CreateSubscription_Domestic_ReturnsDomesticSubscription()
+    {
+        // Arrange
+        var factory = new SubscriptionFactory();
+
+        // Act
+        var subscription = factory.CreateSubscription(Domestic);
+
+        // Assert
+        Assert.IsType<DomesticSubscription>(subscription);
+    }
+
+    [Fact]
+    public void CreateSubscription_Educational_ReturnsEducationalSubscription()
+    {
+        // Arrange
+        var factory = new SubscriptionFactory();
+
+        // Act
+        var subscription = factory.CreateSubscription(Educational);
+
+        // Assert
+        Assert.IsType<EducationalSubscription>(subscription);
+    }
+
+    [Fact]
+    public void CreateSubscription_Premium_ReturnsPremiumSubscription()
+    {
+        // Arrange
+        var factory = new SubscriptionFactory();
+
+        // Act
+        var subscription = factory.CreateSubscription(Premium);
+
+        // Assert
+        Assert.IsType<PremiumSubscription>(subscription);
+    }
+
+    [Fact]
+    public void CreateSubscription_UnknownType_ThrowsNotSupportedException()
+    {
+        // Arrange
+        var factory = new SubscriptionFactory();
+
+        // Act and Assert
+        Assert.Throws<NotSupportedException>(() => factory.CreateSubscription((SubscriptionType)100));
+    }
+
+    [Fact]
+    public void WebSite_CreateSubscription_Domestic_ReturnsDomesticSubscription()
+    {
+        // Arrange
+        var factory = new WebSite();
+
+        // Act
+        var subscription = factory.CreateSubscription(Domestic);
+
+        // Assert
+        Assert.IsType<DomesticSubscription>(subscription);
+    }
+
+    [Fact]
+    public void MobileApp_CreateSubscription_Domestic_ReturnsDomesticSubscription()
+    {
+        // Arrange
+        var factory = new MobileApp();
+
+        // Act
+        var subscription = factory.CreateSubscription(Domestic);
+
+        // Assert
+        Assert.IsType<DomesticSubscription>(subscription);
+    }
+
+    [Fact]
+    public void ManagerCall_CreateSubscription_Domestic_ReturnsDomesticSubscription()
+    {
+        // Arrange
+        var factory = new ManagerCall();
+
+        // Act
+        var subscription = factory.CreateSubscription(Domestic);
+
+        // Assert
+        Assert.IsType<DomesticSubscription>(subscription);
+    }
+}
