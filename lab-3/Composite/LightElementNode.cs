47fac5573990942d423757fcfb566eecca383c05
Oleksandr Karpinskyi <redfield242@gmail.com>
2025-05-10 11:51:41 +0300

    added lifecycle logic (templdte method)

--- lab-3/Composite/LightElementNode.cs
+++ lab-3/Composite/LightElementNode.cs
@@ -1,18 +1,12 @@
 ﻿namespace Composite;
 
-public class LightElementNode(string tagName, string displayType, string closingType, List<string> cssClasses)
-    : LightNode
+public class LightElementNode : LightNode
 {
-    public string TagName { get; } = tagName;
-    public string DisplayType { get; } = displayType;
-    public string ClosingType { get; } = closingType;
-    public List<string> CssClasses { get; } = cssClasses;
-    public List<LightNode> Children { get; } = [];
-
-    public void AddChild(LightNode child)
-    {
-        Children.Add(child);
-    }
+    public string TagName { get; }
+    private string DisplayType { get; }
+    private string ClosingType { get; }
+    private List<string> CssClasses { get; }
+    public List<LightNode> Children { get; }
 
     public override string OuterHtml
     {
@@ -38,7 +32,56 @@ public class LightElementNode(string tagName, string displayType, string closing
     {
         get
         {
-            return Children.Aggregate("", (current, child) => current + child.OuterHtml);
+            return Children.Aggregate("", (current, child) => current + child.Render());
+        }
+    }
+    
+    public LightElementNode(string tagName, string displayType, string closingType, List<string> cssClasses)
+    {
+        TagName = tagName;
+        DisplayType = displayType;
+        ClosingType = closingType;
+        CssClasses = cssClasses;
+        Children = [];
+        OnCreated();
+    }
+    
+    public void AddChild(LightNode child)
+    {
+        Children.Add(child);
+        Console.WriteLine($"Added child element to {TagName}");
+    }
+    
+    public void RemoveChild(LightNode child)
+    {
+        if (!Children.Contains(child)) return;
+        child.Remove();
+        Children.Remove(child);
+        Console.WriteLine($"Removed child element from {TagName}");
+    }
+    
+    protected override void OnCreated()
+    {
+        Console.WriteLine($"ElementNode created: <{TagName}>");
+    }
+    
+    protected override void OnBeforeRender()
+    {
+        Console.WriteLine($"ElementNode preparing to render: <{TagName}>");
+    }
+    
+    protected override void OnAfterRender()
+    {
+        Console.WriteLine($"ElementNode render completed: <{TagName}>");
+    }
+    
+    protected override void OnRemoved()
+    {
+        Console.WriteLine($"ElementNode removed: <{TagName}>");
+        
+        foreach (var child in Children.ToList())
+        {
+            RemoveChild(child);
         }
     }
 }
--- lab-3/Composite/LightNode.cs
+++ lab-3/Composite/LightNode.cs
@@ -4,4 +4,22 @@ public abstract class LightNode
 {
     public abstract string OuterHtml { get; }
     public abstract string InnerHtml { get; }
+    
+    public string Render()
+    {
+        OnBeforeRender();
+        var result = OuterHtml;
+        OnAfterRender();
+        return result;
+    }
+    
+    protected virtual void OnCreated() { }
+    protected virtual void OnBeforeRender() { }
+    protected virtual void OnAfterRender() { }
+    protected virtual void OnRemoved() { }
+    
+    public void Remove()
+    {
+        OnRemoved();
+    }
 }
